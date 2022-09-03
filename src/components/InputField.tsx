9622c307bb85f16b6238ae282e2cd3c91c53d28e
souviksaha27 <souviksahamay27@gmail.com>
2022-09-03 11:58:23 +0530

    Rough Rendering of Tasks done

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./InputField.css";
+import { Todo } from '../model';
 
-const InputField:React.FC = () => {
+const InputField:React.FC = (props) => {
+  
+  const [enteredTask, setenteredTask] = useState('');   //to keep track of entered tasks.
+  const [Todos, setTodos] = useState<Todo[]>([]);        //to keep track of the array of entered tasks
+
+  
+  const addTaskHandler = (event: React.FormEvent<HTMLFormElement>) =>{
+    event.preventDefault();
+    if(enteredTask)
+    {
+      setTodos((prevTask)=>{
+        return [...prevTask, {id: Date.now(), todo: enteredTask , isDone: false}]
+      });
+      setenteredTask('');
+      
+    }
+    
+     
+  }
+
+  const taskChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) =>
+  {
+    setenteredTask(event.target.value);
+  }
+  
   return (
-    <form className='input'>
-      <input type="input" placeholder='Enter a task' className='input__box'/>
+
+    <>
+    {console.log(Todos)}
+    <form className='input' onSubmit={addTaskHandler}>
+      <input type="input" placeholder='Enter a task' value={enteredTask} className='input__box' onChange={taskChangeHandler}/>
       <button className='input_submit' type='submit'>Go</button>
     </form>
+    {Todos.map((t) =>(
+        <li key={t.id}>{t.todo}</li>
+      )
+      )}
+    </>
   )
 }
 export default InputField
